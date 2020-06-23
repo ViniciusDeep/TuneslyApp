6f10501d71a443be90d5514ca8e18a8c11263c14
Vinicius <viniciuscorreia11@gmail.com>
2020-06-22 22:22:39 -0300

    😁 Start project(with new SwiftUI LifeCycle)

---
+++ Tunesly/TuneslyApp.swift
@@ -0,0 +1,23 @@
+//
+//  TuneslyApp.swift
+//  Tunesly
+//
+//  Created by Vinicius Mangueira on 22/06/20.
+//
+
+import SwiftUI
+
+@main
+struct TuneslyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct TuneslyApp_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
